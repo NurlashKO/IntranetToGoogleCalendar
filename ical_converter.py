912b7e1ed11f50d1decaa172226f233d48d01ac9
NurlashKO <zh.nurlan96@gmail.com>
2017-10-27 16:40:04 +0600

    try set time and title

--- ical_converter.py
+++ ical_converter.py
@@ -1,15 +1,18 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from icalendar import Calendar, Event
 
 calendar = Calendar()
 calendar.add('prodid', "KBTU Calendar")
 calendar.add('version', '2.0')
+calendar.add('summary', 'Test calendar name. Kind a long, test test.')
+calendar.add('x-wr-timezone', "Asia/Kazakhstan")
 calendar.add('method', 'publish')
 
 vevent = Event()
 
 vevent.add('summary', "test summary")
 vevent.add('dtstart', datetime.now())
+vevent.add('dtend', datetime.now() + timedelta(hours=3))
 
 calendar.add_component(vevent)
 
