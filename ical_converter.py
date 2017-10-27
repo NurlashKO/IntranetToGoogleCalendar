4aba9075ca0a1bc8a32d785cd3e95016135b0a8a
NurlashKO <zh.nurlan96@gmail.com>
2017-10-27 17:16:47 +0600

    try set time and title

--- ical_converter.py
+++ ical_converter.py
@@ -5,10 +5,11 @@ calendar = Calendar()
 calendar.add('prodid', "-//From NurlashKO//With Love//EN")
 calendar.add('version', '2.0')
 calendar.add('calscale', 'GREGORIAN')
-calendar.add('method', 'publish')
+calendar.add('method', 'PUBLISH')
 calendar.add('x-wr-calname', "KBTU Calendar")
 calendar.add('x-wr-timezone', "Asia/Almaty")
 
+'''
 vevent = Event()
 
 vevent.add('summary', "test summary")
@@ -16,7 +17,7 @@ vevent.add('dtstart', datetime.now())
 vevent.add('dtend', datetime.now() + timedelta(hours=3))
 
 calendar.add_component(vevent)
-
+'''
 f = open('example.ics', 'wb')
 f.write(calendar.to_ical())
 f.close()
