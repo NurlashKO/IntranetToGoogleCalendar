c782347c0c71d2a9e5db91cd79d810b41dabb25e
NurlashKO <zh.nurlan96@gmail.com>
2017-10-31 02:31:26 +0600

    fixed allowed hosts

--- KbtuToCalendar/settings.py
+++ KbtuToCalendar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!7f#-qv$0^hu=&g71^fa=71!g!mc1@y2na0_!7e&pqxhh^1uj1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['warm-temple-53029.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
