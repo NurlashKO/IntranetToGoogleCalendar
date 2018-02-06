d582ecd68aa8f7ff5f8be6b4c1e31a3dea2658b9
NurlashKO <zh.nurlan96@gmail.com>
2018-02-06 16:30:56 +0300

    There was a bug with extra minus sign which lead to wrong week start date calculation

--- app/common/UEvent.py
+++ app/common/UEvent.py
@@ -3,7 +3,7 @@ week_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 class UEvent:
     def __init__(self, title, week_delta, start_time_delta, duration):
         today = date.today()
-        week_start = today - timedelta(days=-today.weekday(), weeks = 1)
+        week_start = today - timedelta(days=today.weekday(), weeks = 1)
         self.title = title.strip()
         self.weekday = week_days.index(week_delta)
         self.start =                                        \
